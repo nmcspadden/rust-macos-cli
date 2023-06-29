9cc33b07c15e1b1c29431e79b058747d3a6b17bb
Nick McSpadden <nmcspadden@gmail.com>
2023-06-28 20:30:59 -0700

    Stole code and now it works

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,45 @@
-use core_foundation_sys::preferences as CFPrefs;
-use core_foundation_sys::string as CFString;
-use core_foundation_sys::propertylist as CFPlist;
-use core_foundation_sys::base as CFBase;
+use core_foundation_sys::{ base::*, string::*, propertylist::* };
 
-use std::ffi::{CString, CStr};
-
-unsafe fn checkpref(key: &str, domain: &str) -> CFPlist::CFPropertyListRef {
-    let application_id = convert_to_cfstring(key);
-    let key = convert_to_cfstring(domain);
-    return CFPrefs::CFPreferencesCopyAppValue(key, application_id);
-}
-
-unsafe fn convert_to_cfstring(input: &str) -> CFString::CFStringRef {
-    let cstr_app_id = CString::new(input).unwrap(); // from a &str, creates a new allocation
-    return CFString::CFStringCreateWithCString(CFBase::kCFAllocatorDefault, cstr_app_id.as_ptr(), CFString::kCFStringEncodingUTF8);
+fn get_setting(key: &str, domain: &str) -> Option<String> {
+    // Stolen from https://github.com/gyroflow/gyroflow/blob/c4241301ff427f9db58bebb0eb271a6bf2d9b9e1/src/core/util.rs#L152
+    unsafe {
+        extern "C" {
+            pub fn CFPreferencesCopyValue(key: CFStringRef, applicationID: CFStringRef, userName: CFStringRef, hostName: CFStringRef) -> CFPropertyListRef;
+        }
+        unsafe fn cfstr(v: &str) -> CFStringRef {
+            CFStringCreateWithBytes(kCFAllocatorDefault, v.as_ptr(), v.len() as CFIndex, kCFStringEncodingUTF8, false as Boolean)
+        }
+        let cfstr_key = cfstr(key);
+        let cfstr_app = cfstr(domain);
+        let user = cfstr("kCFPreferencesAnyUser");
+        let host = cfstr("kCFPreferencesCurrentHost");
+        let ret = CFPreferencesCopyValue(cfstr_key, cfstr_app, user, host);
+        CFRelease(cfstr_key as CFTypeRef);
+        CFRelease(cfstr_app as CFTypeRef);
+        CFRelease(user as CFTypeRef);
+        CFRelease(host as CFTypeRef);
+        if !ret.is_null() {
+            let typ = CFGetTypeID(ret);
+            if typ == CFStringGetTypeID() {
+                let c_string = CFStringGetCStringPtr(ret as CFStringRef, kCFStringEncodingUTF8);
+                if !c_string.is_null() {
+                    let v = std::ffi::CStr::from_ptr(c_string).to_string_lossy().to_string();
+                    CFRelease(ret as CFTypeRef);
+                    return Some(v);
+                } else {
+                    CFRelease(ret as CFTypeRef);
+                }
+            } else {
+                CFRelease(ret as CFTypeRef);
+            }
+        }
+        println!("Result is empty");
+        None
+    }
 }
 
 fn main() {
-    println!("Fuck you");
-    // We need to convert Rust strings to C Strings in order to pass them to CF functions
-    // Thus, we must convert to a CString and then to a CFString
-    let cstr_pref: CFPlist::CFPropertyListRef = unsafe { checkpref("SoftwareRepoURL", "ManagedInstalls") };
-    // What kind of result did we get?
-    // let cstr_result2 = cstr_pref.downcast::<CFString>().unwrap().to_string();
-    // let result_typeid = unsafe { CFBase::CFGetTypeID(cstr_pref) };
-    // println!("pre-crash?");
-    // let cfstring_typieid = unsafe { CFString::CFStringGetTypeID() };
-    // // now to get to figure out how to convert this back to a rust string
-    // // what if we just assume it's a string?
-    // if result_typeid != cfstring_typieid {
-    //     panic!("This is not a string!");
-    // }
-    println!("Crash?");
-    let cstr_result = unsafe { CStr::from_ptr(cstr_pref as *const _) };
-    println!("Crash 2?");
-    let str_result = cstr_result.to_string_lossy();
-    println!("The SoftwareRepoURL for Munki is {str_result}");
+    let str_result = get_setting("SoftwareRepoURL", "ManagedInstalls");
+    let display_str = str_result.as_deref().unwrap_or_default();
+    println!("The SoftwareRepoURL for Munki is {display_str}");
 }
