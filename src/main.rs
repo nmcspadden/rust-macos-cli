550e2f3d4ff7f2770925ac496d3ab828ac623386
Nick McSpadden <nmcspadden@gmail.com>
2023-06-29 20:36:52 -0700

    Added force check

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,24 @@ struct Args {
     key: String,
 }
 
-fn get_setting(key: &str, domain: &str) -> Option<String> {
+fn get_pref_forced(key: &str, domain: &str) -> Boolean {
+    unsafe {
+        extern "C" {
+            pub fn CFPreferencesAppValueIsForced(key: CFStringRef, applicationID: CFStringRef) -> Boolean;
+        }
+        unsafe fn cfstr(v: &str) -> CFStringRef {
+            CFStringCreateWithBytes(kCFAllocatorDefault, v.as_ptr(), v.len() as CFIndex, kCFStringEncodingUTF8, false as Boolean)
+        }
+        let cfstr_key = cfstr(key);
+        let cfstr_app = cfstr(domain);
+        let ret = CFPreferencesAppValueIsForced(cfstr_key, cfstr_app);
+        CFRelease(cfstr_key as CFTypeRef);
+        CFRelease(cfstr_app as CFTypeRef);
+        return ret
+    }
+}
+
+fn get_pref_copyvalue(key: &str, domain: &str, user: &str, host: &str) -> Option<String> {
     // Stolen from https://github.com/gyroflow/gyroflow/blob/c4241301ff427f9db58bebb0eb271a6bf2d9b9e1/src/core/util.rs#L152
     unsafe {
         extern "C" {
@@ -26,8 +43,8 @@ fn get_setting(key: &str, domain: &str) -> Option<String> {
         }
         let cfstr_key = cfstr(key);
         let cfstr_app = cfstr(domain);
-        let user = cfstr("kCFPreferencesCurrentUser");
-        let host = cfstr("kCFPreferencesAnyHost");
+        let user = cfstr(user);
+        let host = cfstr(host);
         let ret = CFPreferencesCopyValue(cfstr_key, cfstr_app, user, host);
         CFRelease(cfstr_key as CFTypeRef);
         CFRelease(cfstr_app as CFTypeRef);
@@ -48,15 +65,91 @@ fn get_setting(key: &str, domain: &str) -> Option<String> {
                 CFRelease(ret as CFTypeRef);
             }
         }
-        println!("Result is empty");
+        // println!("Result is empty");
+        None
+    }
+}
+
+fn get_pref_copyappvalue(key: &str, domain: &str) -> Option<String> {
+    // Stolen from https://github.com/gyroflow/gyroflow/blob/c4241301ff427f9db58bebb0eb271a6bf2d9b9e1/src/core/util.rs#L152
+    unsafe {
+        extern "C" {
+            pub fn CFPreferencesCopyAppValue(key: CFStringRef, applicationID: CFStringRef) -> CFPropertyListRef;
+        }
+        unsafe fn cfstr(v: &str) -> CFStringRef {
+            CFStringCreateWithBytes(kCFAllocatorDefault, v.as_ptr(), v.len() as CFIndex, kCFStringEncodingUTF8, false as Boolean)
+        }
+        let cfstr_key = cfstr(key);
+        let cfstr_app = cfstr(domain);
+        let ret = CFPreferencesCopyAppValue(cfstr_key, cfstr_app);
+        CFRelease(cfstr_key as CFTypeRef);
+        CFRelease(cfstr_app as CFTypeRef);
+        if !ret.is_null() {
+            let typ = CFGetTypeID(ret);
+            if typ == CFStringGetTypeID() {
+                let c_string = CFStringGetCStringPtr(ret as CFStringRef, kCFStringEncodingUTF8);
+                if !c_string.is_null() {
+                    let v = std::ffi::CStr::from_ptr(c_string).to_string_lossy().to_string();
+                    CFRelease(ret as CFTypeRef);
+                    return Some(v);
+                } else {
+                    CFRelease(ret as CFTypeRef);
+                }
+            } else {
+                CFRelease(ret as CFTypeRef);
+            }
+        }
+        // println!("Result is empty");
         None
     }
 }
 
 fn main() {
     let args = Args::parse();
+    /*         
+        {'file': ('/var/root/Library/Preferences/ByHost/'
+                  '%s.xxxx.plist' % domain),
+         'user': kCFPreferencesCurrentUser,
+         'host': kCFPreferencesCurrentHost
+        },
+        {'file': '/var/root/Library/Preferences/%s.plist' % domain,
+         'user': kCFPreferencesCurrentUser,
+         'host': kCFPreferencesAnyHost
+        },
+        {'file': ('/var/root/Library/Preferences/ByHost/'
+                  '.GlobalPreferences.xxxx.plist'),
+         'user': kCFPreferencesCurrentUser,
+         'host': kCFPreferencesCurrentHost
+        },
+        {'file': '/var/root/Library/Preferences/.GlobalPreferences.plist',
+         'user': kCFPreferencesCurrentUser,
+         'host': kCFPreferencesAnyHost
+        },
+        {'file': '/Library/Preferences/%s.plist' % domain,
+         'user': kCFPreferencesAnyUser,
+         'host': kCFPreferencesCurrentHost
+        }, 
+    */
+    let mut str_result = get_pref_copyvalue(&args.key, &args.domain, "kCFPreferencesCurrentUser", "kCFPreferencesAnyHost");
+    let mut display_str = str_result.as_deref().unwrap_or_default();
+    println!("CopyValue: Domain {} / {} = {}", args.domain, args.key, display_str);
 
-    let str_result = get_setting(&args.key, &args.domain);
-    let display_str = str_result.as_deref().unwrap_or_default();
-    println!("Domain {} / {} = {}", args.domain, args.key, display_str);
+    str_result = get_pref_copyvalue(&args.key, &args.domain, "kCFPreferencesCurrentUser", "kCFPreferencesCurrentHost");
+    display_str = str_result.as_deref().unwrap_or_default();
+    println!("CopyValue: Domain {} / {} = {}", args.domain, args.key, display_str);
+
+    str_result = get_pref_copyvalue(&args.key, &args.domain, "kCFPreferencesAnyUser", "kCFPreferencesCurrentHost");
+    display_str = str_result.as_deref().unwrap_or_default();
+    println!("CopyValue: Domain {} / {} = {}", args.domain, args.key, display_str);
+
+    str_result = get_pref_copyvalue(&args.key, &args.domain, "kCFPreferencesAnyUser", "kCFPreferencesAnyHost");
+    display_str = str_result.as_deref().unwrap_or_default();
+    println!("CopyValue: Domain {} / {} = {}", args.domain, args.key, display_str);
+
+    str_result = get_pref_copyappvalue(&args.key, &args.domain);
+    display_str = str_result.as_deref().unwrap_or_default();
+    println!("CopyAppValue: Domain {} / {} = {}", args.domain, args.key, display_str);
+
+    let bool_result = get_pref_forced(&args.key, &args.domain);
+    println!("Value is forced: Domain {} / {} = {}", args.domain, args.key, bool_result);
 }
